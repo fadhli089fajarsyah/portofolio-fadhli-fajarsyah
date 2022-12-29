6e35bb72d5b96cef72637b5355767c0313d23aa2
fadhli089fajarsyah <fadhlifajarsyah089@gmail.com>
2022-12-29 17:51:08 +0700

    update footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="md:p-[80px] p-[10px] text-white bg-gradient-to-r from-blue-900 to-emerald-600 text-[5px] md:text-xs lg:text-sm" >
-        <div class="flex justify-between border-b-2 border-white pb-[30px]">
+    <div class="lg:p-[80px] md:p-[30px] p-[20px] text-white bg-gradient-to-r from-blue-900 to-emerald-600 text-[5px] md:text-xs lg:text-sm" >
+        <div class="flex justify-between  border-b-2 border-white pb-[5px]  md:pb-[30px]">
             <div>              
-                <img class="w-[70%] h-[]" src="../assets/img/Logoft.png" alt="">
+                <img class="w-[60px] md:w-[120px] mb-[4px] lg:w-[170px]" src="../assets/img/Logoft.png" alt="">
                 <p >Ragam kegiatan amal <br> Yayasan Abdi Bakti Izzati <br> dalam rangka membangun <br> individualis/masyarakat <br> muslim yang tangguh dan <br> bermannfaat bagi <br> sekelilingnya.</p>
             </div>
-            <div class="flex flex-col">
-                <p class="mb-[10px] font-bold">Link</p>
+            <div class="flex flex-col ">
+                <p class="md:mb-[10px] mb-[2px] font-bold">Link</p>
                 <a href="" class="link-h" >Beranda</a> 
                 <a href="" class="link-h">Program</a> 
                 <a href="" class="link-h">Layanana</a> 
@@ -15,23 +15,23 @@
                 <a href="" class="link-h"> Tentang kami</a> 
             </div>
             <div class="">
-                <p class="mb-[10px] font-bold">KONTAK</p>
-                <DIv class="flex mb-[5px]">
-                    <svg class="mr-[6px] w-[10px] h-auto " width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <p class="md:mb-[10px] mb-[2px] font-bold">KONTAK</p>
+                <DIv class="flex md:mb-[5px] mb-[1px]">
+                    <svg class="mr-[6px]  w-[5px] h-auto md:w-[12px] " width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M18.3333 14.0999V16.5999C18.3343 16.832 18.2867 17.0617 18.1937 17.2744C18.1008 17.487 17.9644 17.6779 17.7934 17.8348C17.6224 17.9917 17.4205 18.1112 17.2006 18.1855C16.9808 18.2599 16.7478 18.2875 16.5167 18.2666C13.9523 17.988 11.4892 17.1117 9.32498 15.7083C7.31151 14.4288 5.60443 12.7217 4.32499 10.7083C2.91663 8.53426 2.04019 6.05908 1.76665 3.48325C1.74583 3.25281 1.77321 3.02055 1.84707 2.80127C1.92092 2.58199 2.03963 2.38049 2.19562 2.2096C2.35162 2.03871 2.54149 1.90218 2.75314 1.80869C2.9648 1.7152 3.1936 1.6668 3.42499 1.66658H5.92499C6.32941 1.6626 6.72148 1.80582 7.02812 2.06953C7.33476 2.33324 7.53505 2.69946 7.59165 3.09992C7.69717 3.89997 7.89286 4.68552 8.17499 5.44158C8.2871 5.73985 8.31137 6.06401 8.24491 6.37565C8.17844 6.68729 8.02404 6.97334 7.79998 7.19992L6.74165 8.25825C7.92795 10.3445 9.65536 12.072 11.7417 13.2583L12.8 12.1999C13.0266 11.9759 13.3126 11.8215 13.6243 11.755C13.9359 11.6885 14.26 11.7128 14.5583 11.8249C15.3144 12.107 16.0999 12.3027 16.9 12.4083C17.3048 12.4654 17.6745 12.6693 17.9388 12.9812C18.203 13.2931 18.3435 13.6912 18.3333 14.0999Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
 
                     <a href="" class="link-h">0813 8003 534</a>
                 </DIv>
-                <DIv class="flex mb-[5px]">
-                    <svg class="mr-[6px] w-[10px] h-auto  " width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <DIv class="flex md:mb-[5px] mb-[1px]">
+                    <svg class="mr-[6px]  w-[5px] h-auto md:w-[12px]  " width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M16.6666 3.33325H3.33329C2.41282 3.33325 1.66663 4.07944 1.66663 4.99992V14.9999C1.66663 15.9204 2.41282 16.6666 3.33329 16.6666H16.6666C17.5871 16.6666 18.3333 15.9204 18.3333 14.9999V4.99992C18.3333 4.07944 17.5871 3.33325 16.6666 3.33325Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M18.3333 5.83325L10.8583 10.5833C10.601 10.7444 10.3036 10.8299 9.99996 10.8299C9.69636 10.8299 9.3989 10.7444 9.14163 10.5833L1.66663 5.83325" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                    </svg>
                     <a href="" class="link-h">abdibaktiizzati@gmail.com</a>
                 </DIv>
-                <DIv class="flex mb-[5px]">                    
-                    <svg class="mr-[6px] w-[10px] h-auto " width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <DIv class="flex md:mb-[5px] mb-[1px]">                    
+                    <svg class="mr-[6px]  w-[5px] h-auto md:w-[12px] " width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M19.0117 8.33341C19.0117 13.3334 11.4071 18.3334 11.4071 18.3334C11.4071 18.3334 3.80237 13.3334 3.80237 8.33341C3.80237 6.5653 4.60357 4.86961 6.02973 3.61937C7.45589 2.36913 9.39017 1.66675 11.4071 1.66675C13.4239 1.66675 15.3582 2.36913 16.7844 3.61937C18.2105 4.86961 19.0117 6.5653 19.0117 8.33341V8.33341Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M11.4071 10.8333C12.982 10.8333 14.2588 9.71396 14.2588 8.33325C14.2588 6.95254 12.982 5.83325 11.4071 5.83325C9.83207 5.83325 8.5553 6.95254 8.5553 8.33325C8.5553 9.71396 9.83207 10.8333 11.4071 10.8333Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
@@ -43,13 +43,13 @@
             <div>
                 
                 <p class="mb-2">Bantu kami mengubah <br> kehidupan masyarakat <br> yang terkena efek <br> COVID-19</p>
-                <a href="" id="" name="" class="font-bold text-white hover:bg-teal-500 bg-emerald-700 px-2 m-1  md:px-7 md:py-1 rounded">Donasi</a>
+                <a href="" id="" name="" class="font-bold text-white hover:bg-teal-500 bg-emerald-700 px-[20px] py-[2px] md:px-[40px]">Donasi</a>
             </div>
             <div>
                 <p class="mb-[10px] flex justify-center  font-bold">SOSIAL MEDIA</p>
                 <div class="flex justify-between">  
                 <a class="hover:bg-red">                  
-                    <svg width="42" class="px-[2px]" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="42" class="px-[2px] h-auto w-[20px] md:w-[30px] lg:w-[35px]" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="21" cy="21" r="21" fill="white"/>
                         <g clip-path="url(#clip0_32_234)">
                         <path d="M34.4375 14.3203C34.2846 13.775 33.9868 13.2812 33.5757 12.8916C33.153 12.49 32.635 12.2027 32.0705 12.0568C29.9577 11.5 21.4932 11.5 21.4932 11.5C17.9645 11.4598 14.4366 11.6364 10.9295 12.0287C10.3649 12.1853 9.84785 12.479 9.42421 12.8837C9.00796 13.2842 8.70646 13.7781 8.54896 14.3192C8.17062 16.3574 7.98679 18.427 7.99996 20.5C7.98646 22.5711 8.16984 24.64 8.54896 26.6807C8.70309 27.2196 9.00346 27.7112 9.42084 28.1083C9.83821 28.5055 10.358 28.7923 10.9295 28.9442C13.0703 29.5 21.4932 29.5 21.4932 29.5C25.0265 29.5402 28.5588 29.3636 32.0705 28.9712C32.635 28.8254 33.153 28.5381 33.5757 28.1365C33.9866 27.7469 34.2841 27.2531 34.4363 26.7077C34.8246 24.6703 35.0133 22.5999 35 20.5258C35.0292 18.443 34.8406 16.3628 34.4375 14.3192V14.3203ZM18.8022 24.352V16.6491L25.8447 20.5011L18.8022 24.352Z" fill="#0B8457"/>
@@ -62,7 +62,7 @@
                     </svg>
                 </a> 
                 <a class="hover:bg-red">   
-                    <svg width="42" class="px-[2px]" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="42" class="px-[2px] h-auto w-[20px] md:w-[30px] lg:w-[35px]" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="21" cy="21" r="21" fill="white"/>
                         <g clip-path="url(#clip0_32_233)">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M29.896 11H12.104C11.494 11 11 11.494 11 12.104V29.896C11 30.506 11.494 31 12.104 31H21.682V23.255H19.076V20.237H21.682V18.01C21.682 15.426 23.26 14.02 25.565 14.02C26.669 14.02 27.617 14.102 27.894 14.139V16.839H26.296C25.042 16.839 24.8 17.435 24.8 18.309V20.236H27.789L27.399 23.254H24.799V31H29.896C30.506 31 31 30.506 31 29.896V12.104C31 11.494 30.506 11 29.896 11Z" fill="#0B8457"/>
@@ -75,7 +75,7 @@
                     </svg>   
                 </a>  
                 <a class="hover:bg-red">   
-                    <svg width="42" class="px-[2px]" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="42" class="px-[2px] h-auto w-[20px] md:w-[30px] lg:w-[35px]" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="21" cy="21" r="21" fill="white"/>
                         <g clip-path="url(#clip0_32_232)">
                         <path d="M21 17.6667C20.116 17.6667 19.2681 18.0179 18.643 18.6431C18.0179 19.2682 17.6667 20.116 17.6667 21.0001C17.6667 21.8841 18.0179 22.732 18.643 23.3571C19.2681 23.9822 20.116 24.3334 21 24.3334C21.8841 24.3334 22.7319 23.9822 23.357 23.3571C23.9822 22.732 24.3334 21.8841 24.3334 21.0001C24.3334 20.116 23.9822 19.2682 23.357 18.6431C22.7319 18.0179 21.8841 17.6667 21 17.6667Z" fill="#0B8457"/>
