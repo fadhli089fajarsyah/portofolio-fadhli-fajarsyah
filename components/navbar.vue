50bf0c6e4f3337dd5e7a056a82e32ec5b594dc9f
fadhli089fajarsyah <fadhlifajarsyah089@gmail.com>
2022-12-30 17:21:52 +0700

    navbar update

--- components/navbar.vue
+++ components/navbar.vue
@@ -47,7 +47,7 @@
               d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
           </svg>
         </a>
-        <a href="" id="" name=""
+        <a href="donasi" id="" name=""
           class="text-white hover:bg-teal-500 bg-indigo-900 px-2 m-1 ml-[20px]  md:px-7 md:py-1 border rounded ">DONASI</a>
       </div>
     </div>
--- components/ui/HamburgerButton.vue
+++ components/ui/HamburgerButton.vue
@@ -1,15 +1,15 @@
 <template>
-    <button class="relative group" @click="toggleHamburgerButton">
-        <div :class="[active ? 'ring-4' : '']"
-            class="relative flex items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-slate-700 ring-0 ring-gray-300 hover:ring-8 ring-opacity-30 duration-200 shadow-md">
+    <button class="relative group flex px-[10px] flex-col md:hidden relative " @click="toggleHamburgerButton" >
+        <div :class="[active ? 'ring-4' : '']" 
+            class="relative flex items-center justify-center rounded-full w-[30px] h-[30px] transform transition-all bg-white ring-0 ring-gray-300 hover:ring-8 ring-opacity-30 duration-200 shadow-md">
             <div :class="[active ? '-rotate-[45deg]' : '']"
                 class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center">
                 <div :class="[active ? '-rotate-90 h-[1px] translate-y-[1px]' : '']"
-                    class="bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 origin-right delay-75">
+                    class="bg-black h-[2px] w-1/2 rounded transform transition-all duration-300 origin-right delay-75">
                 </div>
-                <div class="bg-white h-[1px] rounded"></div>
+                <div class="bg-black h-[1px] rounded"></div>
                 <div :class="[active ? '-rotate-90 h-[1px] translate-y-[1px]' : '']"
-                    class="bg-white h-[2px] w-1/2 rounded self-end transform transition-all duration-300 origin-left delay-75">
+                    class="bg-black h-[2px] w-1/2 rounded self-end transform transition-all duration-300 origin-left delay-75">
                 </div>
             </div>
         </div>
