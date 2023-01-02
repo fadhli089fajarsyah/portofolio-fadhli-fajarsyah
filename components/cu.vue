55e2bf3231fe168ec12028a23fee390fac6a3209
fadhli089fajarsyah <fadhlifajarsyah089@gmail.com>
2023-01-02 13:44:49 +0700

    menambah swuiper

---
+++ components/cu.vue
@@ -0,0 +1,59 @@
+<template>
+    <swiper
+      :spaceBetween="30"
+      :centeredSlides="true"
+      :autoplay="{
+        delay: 2500,
+        disableOnInteraction: false,
+      }"
+      :pagination="{
+        clickable: true,
+      }"
+      :navigation="true"
+      :modules="modules"
+      class="mySwiper "
+    >
+      <swiper-slide ><img class="w-[100%]" src="../assets/img/nb.png" alt=""></swiper-slide>
+      <swiper-slide ><img class="w-[100%]" src="../assets/img/nb.png" alt=""></swiper-slide>
+      <swiper-slide ><img class="w-[100%]" src="../assets/img/nb.png" alt=""></swiper-slide>
+    </swiper>
+  </template>
+  <script>
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from "swiper/vue";
+  
+  // Import Swiper styles
+  import "swiper/css";
+  
+  import "swiper/css/pagination";
+//   import "swiper/css/navigation";
+  
+//   import "./style.css";
+  
+  // import required modules
+  import { Autoplay, Pagination, Navigation } from "swiper";
+  
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [Autoplay, Pagination, Navigation],
+      };
+    },
+  };
+  </script>
+
+<style>
+.swiper-button-prev{
+    opacity: 0;
+}
+.swiper-button-disabled{
+    opacity: 0;
+}
+.swiper-button-next{
+    opacity: 0;
+}
+</style>
---
+++ pages/cn.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- slider container container container (to add dot nav btns below) -->
+  <div>
+      <!-- slider container container (to add nav btns) -->
+      <div class="flex items-center gap-x-8">
+          <!-- prev btn -->
+          <button class="text-black text-2xl" @click="prev">&larr;</button>
+
+          <!-- slider container -->
+          <div class="w-[500px] overflow-hidden">
+              <!-- the slide items will collapse horizontally -->
+              <div class="flex items-center">
+                  <!-- img container -->
+                  <div v-for="number in 1" :key="'pic-' + 1" class="w-[500px] min-w-[500px] max-w-[500px]"
+                      :style="{ transform: `translateX(${-500 * (currentSlide - 1)}px)` }">
+                      <img src="../assets/img/nb.png" :alt="' ' + number" />
+                  </div>
+                  <div v-for="number in 2" :key="'pic-' + 2" class="w-[500px] min-w-[500px] max-w-[500px]"
+                      :style="{ transform: `translateX(${-500 * (currentSlide - 1)}px)` }">
+                      <img src="../assets/img/1.png" :alt="' ' + number" />
+                  </div>
+              </div>
+          </div>
+
+          <!-- next btn -->
+          <button class="text-black text-2xl" @click="next">&rarr;</button>
+      </div>
+
+      <!-- dots -->
+      <div class="flex items-center justify-center gap-x-4 py-4">
+          <button v-for="number in 3" :key="'slider2-dot-' + number" class="h-4 rounded-full" :class="{
+  'bg-black w-4': currentSlide !== number,
+  'bg-blue-500 w-12': currentSlide === number,
+}" @click="currentSlide = number" />
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          currentSlide: 1,
+      }
+  },
+  methods: {
+      prev() {
+          if (this.currentSlide > 1) {
+              this.currentSlide--
+          } else {
+              this.currentSlide = 3
+          }
+      },
+      next() {
+          if (this.currentSlide === 3) {
+              this.currentSlide = 1
+          } else {
+              this.currentSlide++
+          }
+      },
+  },
+}
+</script>
